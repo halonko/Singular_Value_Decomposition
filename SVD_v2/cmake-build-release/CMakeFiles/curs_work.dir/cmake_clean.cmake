744018fe6657d6ff3917af70231265caff220625
popenyuk <popenyuk@ucu.edu.ua>
2019-05-14 21:35:02 +0300

    Done

---
+++ SVD_v2/cmake-build-release/CMakeFiles/curs_work.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curs_work.dir/main.cpp.o"
+  "CMakeFiles/curs_work.dir/Sources/functions.cpp.o"
+  "CMakeFiles/curs_work.dir/Sources/my_matrix.cpp.o"
+  "CMakeFiles/curs_work.dir/Sources/read_functions.cpp.o"
+  "curs_work.pdb"
+  "curs_work"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curs_work.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SVD_v2/cmake-build-release/CMakeFiles/curs_work.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for curs_work.
+# This may be replaced when dependencies are built.
